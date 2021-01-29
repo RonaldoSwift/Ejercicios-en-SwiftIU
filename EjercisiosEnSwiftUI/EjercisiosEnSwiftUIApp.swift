61c29a78cdeee44d4d517f3f39ba1d4e6436a120
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2021-01-29 10:04:33 -0500

    first commit

---
+++ EjercisiosEnSwiftUI/EjercisiosEnSwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  EjercisiosEnSwiftUIApp.swift
+//  EjercisiosEnSwiftUI
+//
+//  Created by Ronaldo Andre Vargas Huaman on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct EjercisiosEnSwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
