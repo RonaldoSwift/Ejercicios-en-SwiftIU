44d66c7c7feac6877dc473c4809053832cb6b116
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2021-02-04 18:46:01 -0500

    Ejercicio4

--- EjercisiosEnSwiftUI/EjercisiosEnSwiftUIApp.swift
+++ EjercisiosEnSwiftUI/EjercisiosEnSwiftUIApp.swift
@@ -13,7 +13,7 @@ struct EjercisiosEnSwiftUIApp: App {
 
     var body: some Scene {
         WindowGroup {
-            Pantalla4View()
+            Pantalla5View()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
