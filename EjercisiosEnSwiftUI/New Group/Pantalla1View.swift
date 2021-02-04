65cea1cc079d4bd1bc9bd97784a345bb04cfdc78
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2021-02-04 10:17:43 -0500

    agregando spacer

--- EjercisiosEnSwiftUI/New Group/Pantalla1View.swift
+++ EjercisiosEnSwiftUI/New Group/Pantalla1View.swift
@@ -11,26 +11,22 @@ struct Pantalla1View: View {
     var body: some View {
         NavigationView{
             VStack{
-                
                 Text("TITLE")
                     .foregroundColor(.black)
-                    .background(Color.gray)
                     .truncationMode(.middle)
                     .font(.largeTitle)
-                
-                    
+                    .multilineTextAlignment(.center)
                 Spacer()
                 
                 Text("Traducción del inglés-El título de un libro, o cualquier otro texto u obra de arte publicada, es un nombre para la obra que generalmente elige el autor. Se puede utilizar un título para identificar el trabajo, ubicarlo en contexto, transmitir un resumen mínimo de su contenido y despertar la curiosidad del lector.")
-                    .background(Color.gray)
                     .lineSpacing(20)
-                
-                Spacer()
+                    .multilineTextAlignment(.center)
+                     Spacer()
         
                NavigationLink("Continuar", destination: Pantalla2View())
-                .navigationTitle("TITLE")
                 .foregroundColor(Color.blue)
                 .font(.largeTitle)
+                Spacer()
             }
         }
     }
