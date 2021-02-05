62f4ce20ef900c38c42c91d002edf5169b54ee8a
Ronaldo Andre Vargas Huaman <ronaldovh99@gmail.com>
2021-02-05 11:44:04 -0500

    Ejercicio 6

---
+++ EjercisiosEnSwiftUI/Ejercicio5/Pantalla6View.swift
@@ -0,0 +1,63 @@
+//
+//  Pantalla6View.swift
+//  EjercisiosEnSwiftUI
+//
+//  Created by Ronaldo Andre Vargas Huaman on 2/5/21.
+//
+
+import SwiftUI
+
+struct CircleHumanView: View {
+    var body: some View {
+        ZStack{
+            Color.blue
+            VStack {
+                Text("BODY HUMAN")
+                    .font(.body)
+                    .foregroundColor(.black)
+                
+                Image("Human")
+                    .resizable()
+                    .frame(width: 210.0, height: 250.0, alignment: .center)
+                    .clipShape(Circle())
+            }
+        }
+        
+    }
+}
+
+struct WelcomeView: View{
+    var body: some View {
+        ZStack{
+            Color.green
+            VStack {
+                Spacer()
+                Button(action: {
+                        print("WELCOMEEE!!!")}){
+                    HStack{
+                        Text("WELCOME->")
+                    }
+                    .foregroundColor(Color.black)
+                    .padding(30.0)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20.0)
+                            .stroke(lineWidth: 3.0))
+                }
+                .foregroundColor(Color.black)
+            }
+        }
+    }
+}
+
+struct Pantalla6View: View {
+    var body: some View {
+        CircleHumanView()
+        WelcomeView()
+    }
+}
+
+struct Pantalla6View_Previews: PreviewProvider {
+    static var previews: some View {
+        Pantalla6View()
+    }
+}
