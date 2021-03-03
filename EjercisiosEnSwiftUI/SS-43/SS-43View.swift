f0b4c31bd364a0028bd41d9af25c5f4c34e87b67
Ronaldo Vargas <ronaldovh99@gmail.com>
2021-03-03 18:35:22 -0500

    Cambios de SS-43

--- EjercisiosEnSwiftUI/SS-43/SS-43View.swift
+++ EjercisiosEnSwiftUI/SS-43/SS-43View.swift
@@ -8,90 +8,92 @@
 import SwiftUI
 
 struct SS_43View: View {
+    var opciones: [Opcion] = [
+        Opcion(id: 0, imagen: "list.dash", texto: "Current List", color: Color.blue),
+        Opcion(id: 1, imagen: "text.badge.plus", texto: "My Lists", color: Color.yellow),
+        Opcion(id: 2, imagen: "magnifyingglass", texto: "Search Products", color: Color.black),
+        Opcion(id: 3, imagen: "message", texto: "Feed Back", color: Color.init("CustonGreen"))]
     var body: some View {
         VStack{
-            cabezeraView()
-            nombreList()
-        }
-    }
-    
-    func cabezeraView() -> some View {
-        return ZStack{
-            Image("Lista-De-Compras")
-            VStack{
-                Text("Header")
-                    .font(.title)
-                    .multilineTextAlignment(.leading)
-                    .foregroundColor(Color.red)
-                    .frame(width: 100, height: 50, alignment: .center)
-                Text("Soriana")
-                    .font(.title2)
-                    .multilineTextAlignment(.leading)
-                    .foregroundColor(Color.white)
-                    .frame(width: 400, height: 50, alignment: .leading)
-                Text("Av.Eugenio Garza Sada 745")
-                    .font(.title2)
-                    .multilineTextAlignment(.leading)
-                    .foregroundColor(Color.white)
-                    .frame(width: 400, height: 50, alignment: .leading)
+            Section(header: headerView(), footer: footerView()) {
+                contentView()
             }
         }
     }
     
-    func nombreList() -> some View{ 
-        return NavigationView{
-            List{
-                Section(header: HStack{
-                    Image(systemName: "list.dash")
-                    Text("Current List")
-                        .font(.headline)
-                        .foregroundColor(Color.blue)
-                        .frame(width: 100, height: 80, alignment: .center)
-                }){
-                    
-                }
-                Section(header: HStack{
-                    Image(systemName: "text.badge.plus")
-                    Text("My Lists")
-                        .font(.headline)
-                        .frame(width: 100, height: 80, alignment: .center)
-                }){
-                    
-                }
-                Section(header: HStack{
-                    Image(systemName: "magnifyingglass")
-                    Text("Search Products")
-                        .font(.headline)
-                        .frame(width: 100, height: 80, alignment: .center)
-                }){
-                    
-                }
-                Section(header: HStack{
-                    Image(systemName: "message")
-                    Text("Feed Back")
-                        .font(.headline)
-                        .frame(width: 100, height: 80
-                               , alignment: .center)
-                }){
-                    
-                }
-                Section(footer: HStack{
-                    Image("Persona1")
-                        .resizable()
-                        .frame(width: 50, height: 80, alignment: .center)
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.black, lineWidth: 2.0))
-                    HStack{
-                        Text("Sign Out")
-                            .font(.headline)
-                        Text("footer")
-                            .foregroundColor(Color.red)
+    private func headerView() -> some View {
+        return
+            ZStack{
+                Image("Lista-De-Compras")
+                    .resizable()
+                    .border(Color.blue, width: 3.0)
+                HStack{
+                    VStack(alignment: .leading){
+                        Spacer()
+                        Image("Login")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                        Spacer()
+                        Text("Soriana")
+                            .font(.title2)
+                            .bold()
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(Color.white)
+                        
+                        Text("Av.Eugenio Garza Sada 745")
+                            .font(.title2)
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(Color.white)
                     }
-                }){
+                    Spacer()
                 }
+                .padding(10)
+            }
+            .frame(height: 200)
+    }
+    
+    private func contentView() -> some View{
+        return List{
+            ForEach(opciones, id: \.id){ opcion in
+                    
+                    HStack{
+                        Image(systemName: opcion.imagen)
+                        Text(opcion.texto)
+                            .bold()
+                            .font(.title2)
+                            .padding()
+                    }
+                    .foregroundColor(opcion.color)
+               
             }
         }
     }
+    
+    private func footerView() -> some View {
+        return
+            HStack{
+                Image("Persona1")
+                    .resizable()
+                    .frame(width: 50, height: 50, alignment: .leading)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.black, lineWidth: 2.0))
+                Text("Sign Out")
+                    .font(.title2)
+                    .bold()
+                    .font(.headline)
+                    .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
+                Spacer()
+            }
+            .padding(10)
+    }
+}
+
+struct Opcion: Identifiable {
+    var id: Int
+    var imagen: String
+    var texto: String
+    var color: Color
+    
 }
 
 struct SS_43View_Previews: PreviewProvider {
