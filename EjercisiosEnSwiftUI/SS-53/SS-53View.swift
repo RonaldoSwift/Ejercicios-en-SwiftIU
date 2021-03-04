d870c0780f9944016396119115de99de809840eb
Ronaldo Vargas <ronaldovh99@gmail.com>
2021-03-04 10:34:40 -0500

    Resolviendo Image

--- EjercisiosEnSwiftUI/SS-53/SS-53View.swift
+++ EjercisiosEnSwiftUI/SS-53/SS-53View.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 struct SS_53View: View {
     var tarjetas: [Tarjetas] = [
-        Tarjetas(id: 0, nombre: "Anxiety", descripcion: "Turn down the stress volume", image: "Apple", fecha: "7 sep", hora: "5 - 11 min"),
-        Tarjetas(id: 1, nombre: "Ronaldo", descripcion: "Turn down the stress volume", image: "Apple", fecha: "19 ago", hora: "7-10 min"),
-        Tarjetas(id: 2, nombre: "Eduard", descripcion: "Turn down the stress volume", image: "Apple", fecha: "27 mar", hora: "8 - 9 min"),
-        Tarjetas(id: 3, nombre: "Juan", descripcion: "Turn down the stress volume", image: "Apple", fecha: "31 jun", hora: "4 - 6 min")]
+        Tarjetas(id: 0, nombre: "Anxiety", descripcion: "Turn down the stress volume", image: "Apple", fecha: "7 sep", hora: "/ 5 - 11 min", color: Color.blue),
+        Tarjetas(id: 1, nombre: "Ronaldo", descripcion: "Turn down the stress volume", image: "Apple", fecha: "19 ago", hora: "/7- 10 min", color: Color.green),
+        Tarjetas(id: 2, nombre: "Eduard", descripcion: "Turn down the stress volume", image: "Apple", fecha: "27 mar", hora: "/8 - 9 min", color: Color.orange),
+        Tarjetas(id: 3, nombre: "Juan", descripcion: "Turn down the stress volume", image: "Apple", fecha: "31 jun", hora: "/ 4 - 6 min", color: Color.red)]
     
     var body: some View {
         moverTarjetas()
@@ -23,38 +23,42 @@ struct SS_53View: View {
             ScrollView(.horizontal, showsIndicators: false){
                 HStack{
                     ForEach(tarjetas, id: \.id){ tarjeta in
-                        VStack{
+                        VStack(alignment: .leading){
+                            
                             Text(tarjeta.nombre)
                                 .font(.title)
+                                .bold()
                                 .foregroundColor(Color .white)
-                            
+                                .background(Color.green)
+
                             Text(tarjeta.descripcion)
                                 .font(.title2)
                                 .foregroundColor(Color .white)
+                                .background(Color.orange)
+                            
                             HStack{
                             Text(tarjeta.fecha)
                                 .font(.title3)
                                 .foregroundColor(Color .white)
+                                .background(Color.purple)
                             
                             Text(tarjeta.hora)
                                 .font(.title3)
                                 .foregroundColor(Color .white)
+                                .background(Color.yellow)
+
+                            Image(tarjeta.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 120, height: 90, alignment: .center)
+                                .clipShape(Circle())
+                                .overlay(Circle().stroke(Color.black, lineWidth: 6))
+                                .background(Color.red)
                             }
-                            HStack{
-                                Image(tarjeta.image)
-                                    .resizable().scaledToFit()
-                                    .frame(width: 100, height: 100, alignment: .leading)
-                                    .clipShape(Circle())
-                                    .overlay(Circle().stroke(Color.black, lineWidth: 6))
-                                    .shadow(color: .blue, radius: 20, x: 1, y: 1)
-                            }
-                            
-                        }.padding(15)
+                        }.padding(20)
                         .foregroundColor(Color.white)
                         .background(Color.blue)
-                        .frame(height: 250)
                         .cornerRadius(25)
-                        
                     }
                 }
             }.padding(10)
@@ -68,6 +72,7 @@ struct Tarjetas: Identifiable{
     var image: String
     var fecha: String
     var hora: String
+    var color: Color
 }
 
 struct SS_53View_Previews: PreviewProvider {
